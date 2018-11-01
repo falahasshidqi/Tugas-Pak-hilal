d475dba622758ec43fc3e968442b2e0257f9c603
falahasshidqi <43982309+falahasshidqi@users.noreply.github.com>
2018-11-01 21:53:43 +0700

    Add files via upload

---
+++ tugaspiramidbelahketupatsegitiga.php
@@ -0,0 +1,74 @@
+
+
+<?php
+
+for( $a=10;$a>0;$a--){
+for($i=1; $i<=$a; $i++){
+echo "&nbsp ";
+
+}
+for($a1=10;$a1>$a;$a1--){
+echo"*";
+}
+for($a2=10;$a2>$a;$a2--){
+echo"*";
+}
+echo"<br>";
+}
+
+
+
+
+?>
+
+<br>
+<?php
+
+for( $a=10;$a>0;$a--){
+for($i=1; $i<=$a; $i++){
+echo "&nbsp ";
+
+}
+for($a1=10;$a1>$a;$a1--){
+echo"*";
+}
+for($a2=10;$a2>$a;$a2--){
+echo"*";
+}
+echo"<br>";
+}
+for($b=0; $b<=10;$b++){
+for($j=1;$j<=$b;$j++){
+echo"&nbsp ";
+}
+for($b1=10; $b1>$b;$b1--){
+echo"*";
+}
+for($b2=10; $b2>$b;$b2--){
+echo"*";
+}
+echo"<br>";
+}
+
+
+?>
+<br>
+
+<?php
+
+for( $a=10;$a>0;$a--){
+for($i=1; $i<=$a; $i++){
+echo "  ";
+
+}
+for($a1=10;$a1>$a;$a1--){
+echo"*";
+}
+for($a2=10;$a2>$a;$a2--){
+echo"*";
+}
+echo"<br>";
+}
+
+
+?>
